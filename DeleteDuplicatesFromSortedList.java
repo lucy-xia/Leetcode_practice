4ab510440b2f39c26a897911964409d68cacdf28
lucy-xia <lucy.xia@yahoo.com>
2021-04-05 14:02:42 +0800

    double checked

--- DeleteDuplicatesFromSortedList.java
+++ DeleteDuplicatesFromSortedList.java
@@ -1,18 +1,19 @@
 public int DeleteDuplicatesFromSortedList(ListNode head){
 	//重複的點只保留一個值
-	if(head==null || head.next==null)
-		return head;
-
-	ListNode current_node = head;
-
-	while(current_node!=null && current_node.next !=null){
-		if(current_node.val=current_node.next.val){
-			current_node.next=current_node.next.next;
-		}
-		else
-			current_node= current_node.next;
-	}
-	return head;
+	   ListNode prev=head;
+        
+       // if(prev=null || prev.next=null)
+       //     return head; //報錯
+                         //first type:  <null>
+                         //second type: ListNode
+                         
+        while(prev!= null && prev.next!=null){
+            while(prev.val==prev.next.val){
+                prev.next= prev.next.next;
+            }
+            prev= prev.next;
+        }
+        return head;
 }
 
 public int DeleteDuplicatesFromSortedListII(ListNode head){
@@ -20,8 +21,8 @@ public int DeleteDuplicatesFromSortedListII(ListNode head){
 	ListNode dummy_head = new ListNode(0,head);
 	ListNode prev = dummy_head; 
 	while(head != null){
-		if(dummy_head != null && dummy_head.val = dummy_head.next.val){
-			while(dummy_head != null && dummy_head.val = dummy_head.next.val){
+		if(head != null && head.val = head.next.val){
+			while(head != null && head.val = head.next.val){
 				head = head.next; //持續遍歷值相同的點
 			}
 			prev.next = head.next; //skip over那些相同的點
@@ -29,7 +30,10 @@ public int DeleteDuplicatesFromSortedListII(ListNode head){
 		else{ //將那些值不同的點收入囊中
 			prev = prev.next;
 		}
-		head = head.next; //保持向前移動
+		head = head.next; //保持向前移動,到目前為止值相同的點都遍歷完了，所以要開始吃新的點
 	}
 	return dummy_head.next;
 }
+
+
+
--- RemoveDuplicatesFromSortedArray.java
+++ RemoveDuplicatesFromSortedArray.java
@@ -1,19 +1,21 @@
 public int RemoveDuplicates(int[] nums){
-	int i=0;
+
+	int i=0; //in是可以被改變位置的候選人
 	for(int j=0;j< nums.length;j++){
 		if(nums[i]!= nums[j]){
 			num[++i]=num[j];
 		}
-	return i+1;
 	}
+	return i+1;
 }
 
 public int RemoveDuplicatesII(int[] nums){
 	if(nums.length<3) return nums.length;
-	int in=2;
-	for(int i=2; i<nums.length;i++){
-		if(nums[in-2]!=nums[i]){
-			nums[in++]=nums[i];
+
+	int i=2; //in是可以被改變位置的候選人
+	for(int j=2; j<nums.length;j++){
+		if(nums[i-2]!=nums[j]){
+			nums[i++]=nums[j];
 		}
 	}
 	return in;
