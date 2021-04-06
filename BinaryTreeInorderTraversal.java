a192a7debc64b617120a3044e19c84b70ccd7a2f
lucy-xia <lucy.xia@yahoo.com>
2021-04-06 16:19:28 +0800

    Inorder traversal recursive/iterative method

---
+++ BinaryTreeInorderTraversal.java
@@ -0,0 +1,28 @@
+//recursive method
+public List<Integer> inorderTraversal(TreeNode root){
+	return dfs(root, new ArrayList());
+}
+private dfs (TreeNode roof, List<Integer> list){
+	if(root == null){
+		return null;
+	}
+	list= dfs(root.left, list); // Traverse the left subtree
+	list.add(root.val); // Record the current node
+	return dfs(root.right,list); // Traverse the right subtree
+}
+
+
+//iterative method
+public List<Integer> inorderTraversal(TreeNode root){
+	Stack<Integer> list= new ArrayList<>(); // create an empty stack
+	while(!stack.isEmpty() || root!=null){
+		while(root!=null){
+			stack.push(root); // if the current node exists, push it into the stack (defer it)
+			root= root.left; // move to its left child
+		}
+		root= stack.pop(); //if the current node is null, pop an element from the stack, print it
+		list.add(root.val);
+		root=root.right; //set the current node to its right child
+	}
+	return list;
+}
