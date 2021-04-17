187aaef02cc5e050bac363e6e64ec63f550e1b64
lucy-xia <lucy.xia@yahoo.com>
2021-04-17 10:33:46 +0800

    addBinary.java char不能強制轉型為int

---
+++ addBinary.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder sb= new StringBuilder(); 
+        int p1= a.length() -1 ; //因為要包含0，所以
+        int p2= b.length() -1 ;
+
+        int carry=0; //要進位的小數
+        while(p1>=0 || p2>=0){
+        	int num1=p1>=0?(a.charAt(p1)-'0'):0; //要從最高一位遍歷到charAt[0]這個位置，所以迴圈要取到0
+        	int num2=p2>=0?(b.charAt(p2)-'0'):0; //將char轉變為int      //Q:why does using (int) does not work?
+        	int sum=num1+num2+carry;                                   //A:因為數字字元的ascii碼是從48開始，char強制轉型為int會產生錯誤。
+        	sb.insert(0,sum%2);
+        	carry =sum/2;
+        	p1--;
+        	p2--;
+
+        }
+        if(carry!=0)
+        	sb.insert(0,1);
+        return sb.toString();
+    } 
+}
