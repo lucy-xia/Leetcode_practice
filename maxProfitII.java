0b55814a5a0f3f4262e54f8b5fb7f47f7fc442a5
lucy-xia <lucy.xia@yahoo.com>
2021-04-26 16:33:27 +0800

    122. Best Time to Buy and Sell Stock II

--- maxProfitII.java
+++ maxProfitII.java
@@ -16,3 +16,17 @@ class Solution {
         return result;
     }
 }
+
+/* Better solution
+class Solution {
+    public int maxProfit(int[] prices) { // 把峰值加起来 平滑的和下降的都不做考虑
+        if(prices==null || prices.length==1) return 0;
+        int result =0;
+        for(int i=1; i<prices.length; i++){
+            if(prices[i]>prices[i-1])
+                result+=(prices[i]-prices[i-1]);
+        }        
+        return result;
+    }
+}
+*/
