cd892366d88a783cf53e7be42efbce1aebb31255
lucy-xia <lucy.xia@yahoo.com>
2021-04-29 11:09:10 +0800

    160. Intersection of Two Linked Lists

--- getIntersectionNode.java
+++ getIntersectionNode.java
@@ -1,3 +1,11 @@
+/*
+利用x+Z+Y=Y+Z+X的思路：
+分別講AB各自走一邊
+跳出while迴圈的時候是都各自走到list相交的部分
+如果沒有共同的list部分則在最後都到達第二個list的最後
+注：這裡需要重新開啟一個單列因為原本的headA/B需要被重新用到，不能把他洗壞了
+*/
+
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         if(headA==null|| headB==null) return null;
