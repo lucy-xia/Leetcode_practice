611c0baafb1ea714b3ed3a771a58a0bf4dd3b0db
lucy-xia <lucy.xia@yahoo.com>
2021-05-14 20:00:40 +0800

    116. Populating Next Right Pointers in Each Node

--- connect.java
+++ connect.java
@@ -1,37 +1,25 @@
+//Initially, all next pointers are set to NULL.
+
 class Solution {
     public Node connect(Node root) {
-        Node cur= root; //cur是上面一層的點
-        Node pre= null; //pre是下面一層正在進行連接操作的點
-        Node head= null;  //head是下一層的第一個點，用來保存（頭）
+    	if(root== null) 
+    		return root;
+    	Node cur= root;
 
-        While(cur!=null){
-        	while(cur!=null){
-        		if(cur.left!=null){
-        			if(head==null){
-        				head=cur.left;
-        				pre=cur.left;
-        			}
-        			else{
-        				pre.next=cur.left;
-        				pre=pre.next;
-        			}
-        		}
-        		if(cur.right!=null){ 
-        			if(head==null){
-        				head=cur.left;
-        				pre=cur.left;
-        			}
-        			else{
-        				pre.next=cur.left;
-        				pre=pre.next;
-        			}
-        		}
-        		cur=cur.next;
-        	}
-        	cur=head;
-        	Node head= null;
-        	Node pre= null;
-        }
-        return root;
+    	while(cur!=null){
+    		Node head= cur;
+    		while(cur!=null){
+    			if(cur.left!=null){
+    				cur.left.next=cur.right;
+    			}
+    			if(cur.right!=null && cur.next!=null){
+    				cur.right.next= cur.next.left;
+    			}
+    			cur=cur.left;
+    		}
+    		cur=head.left;
+    	}
+    	return root;
+    	
     }
 }
