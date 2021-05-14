b932bc9c17c31ba8397248c18ddd5d40a168fe89
lucy-xia <lucy.xia@yahoo.com>
2021-05-14 23:31:04 +0800

    226. Invert Binary Tree

---
+++ invertTree.java
@@ -0,0 +1,17 @@
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        
+        if(root == null){
+            return null;
+        }
+                
+        TreeNode left= invertTree(root.left);
+        TreeNode right= invertTree(root.right);
+        
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        
+        return root;
+    }
+}
