289a76ac788f4cc963cc8b40cdc530beb5eae9bf
lucy-xia <lucy.xia@yahoo.com>
2021-05-16 15:48:40 +0800

    119. Pascal's Triangle II

--- getRow.java
+++ getRow.java
@@ -1,9 +1,9 @@
 class Solution {  
     public List<Integer> getRow(int rowIndex) {
     	List<Integer> result= new ArrayList();
-    	for(int i=0; i <= rowIndex; i++){
+    	for(int i=0; i <= rowIndex; i++){ //要取到等号
     		result.add(0,1); //index, value
-    		for(int j=1; j< result.size(); j++){
+    		for(int j=1; j< result.size()-1; j++){ //size 而不是 size()
     			result.set(j, result.get(j)+result.get(j+1));
     		}
     	}
