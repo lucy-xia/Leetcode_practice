576c8a141d026fdb16699214209190396fb883df
lucy-xia <lucy.xia@yahoo.com>
2021-05-17 16:36:05 +0800

    81. Search in Rotated Sorted Array II

---
+++ findMin.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findMin(int[] nums) {
+    	int left=0;
+        int right= nums.length-1;
+        
+        while(left+1<right){  //1+1<3
+            int mid=(left+right)/2;
+            if(nums[mid]>nums[right]) 
+                left=mid;        
+            else if(nums[mid]<nums[right])
+            	right=mid;
+        }
+   
+        if(nums[left]<nums[right]){
+            return nums[left];
+        else 
+        	return nums[right];
+        }            
+    }
+}
