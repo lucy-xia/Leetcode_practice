2d73ee4b2a4082fd90f4dd9cf25b6416ab6cbdfc
lucy-xia <lucy.xia@yahoo.com>
2021-05-27 20:03:22 +0800

    8. String to Integer (atoi)

---
+++ myAtoi.java
@@ -0,0 +1,42 @@
+//atoi=ASCII to Integer 字符轉數字
+class Solution {
+    public int myAtoi(String s) {
+        int i=0;
+
+        //1.略過空值
+        while(i<s.length() && s.charAt(i)== ' ') //從前找到後若是空格就跳過
+        	i++;
+        if(i==s.length()) //找到最後發現到底的話，就回傳0
+        	return 0;
+
+        //2.判斷正負
+        int inNegative =1;
+        if(s.charAt(i)== '+' || s.charAt(i)=='-'){
+        	if(s.charAt(i)=='-')
+        		inNegative=-1;
+        	i++;
+        }
+        if(i==s.length()) //找到最後發現到底的話，就回傳0
+        	return 0;
+
+        //3.check是否是數字
+        int result=0;
+        while(i<s.length(0)){
+        	char c=s.char(i);
+        	if(c<'0'||c>'9') //stops if that's not integer
+        		return result*isNegative;
+
+        //4.以上check都沒有問題的話，就開始建造result		
+        	int num=c-'0';
+        	int tmp= result*10+num;
+
+        //5.check the bound of Integer
+        	if(tmp/10!= result)
+        		return inNegative==-1? Integer.MIN_VALUE: Integer/MAX_VALUE:
+        	result=tmp;
+        	i++;
+
+        }
+        return result*isNegative;
+    }
+}
