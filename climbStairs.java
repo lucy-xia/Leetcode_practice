4d59b8302b4448e4d1cde4d7e3596f2c8dad51fc
lucy-xia <lucy.xia@yahoo.com>
2021-07-24 23:24:52 +0800

    70. Climbing Stairs

--- climbStairs.java
+++ climbStairs.java
@@ -1,37 +1,30 @@
 class Solution {
     public int climbStairs(int n) {
-    /*
-    //brute force]
-    if(n==0) 
-        return 0;
-    if(n==1) 
-        return 1;
-      if(n==2) 
-        return 2;
-    else 
-        return climbStairs(n-1)+climbStairs(n-2);
-
-    //this requires extra places
-    int[] res=new int[n+1];
-    res[0]=1;
-    res[1]=1;
-    if(n>=2){
-        for(int i=2;i<n+1;i++){
-            res[i]=res[i-1]+res[i-2];
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i=2; i<=n; i++) {
+                dp[i] = dp[i - 1]+dp[i - 2]; //因为每次只能爬 11 级或 22 级，所以 f(x)f(x) 只能从 f(x - 1)f(x−1) 和 f(x - 2)f(x−2) 转移过来
         }
+        return dp[n];
     }
-    return res[n];
-     */
-    
-    //this can save extra places
-    if(n==1) return n;
-    int onestep = 1;
-    int twostep = 2;
-    for(int i=3; i<=n; i++){
-        int tmp = twostep;
-        twostep= twostep+ onestep;
-        onestep= tmp;
-    }
-    return twostep;
+}
+
+//脑洞大开，泛化可以爬的阶梯数【排列问题】
+class Solution {
+    public int climbStairs(int n, int[] step) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i=2; i<=n; i++) {
+        	for(int j=0; j<step.length ; j++){
+        		int step= step[j];
+        		if(step > i) //可以跨越的步数大于台阶数
+        			continue;
+                dp[i] = dp[i]+dp[i-step];
+        	}
+        		
+        }
+        return dp[n];
     }
 }
